9f92218486a0aa98f0691bf9dce6e6f749c73a38
agastyawan <161032211+agastyawan@users.noreply.github.com>
2024-03-11 15:06:08 +0000

    Create load_data.R

---
+++ R/load_data.R
@@ -0,0 +1,6 @@
+library(readr)
+library(RSQLite)
+
+customer_data <- readr::read_csv("../data_upload/customer.csv")
+my_connection <-RSQLite::dbConnect(RSQLite(),"../database/e-commerce.db")
+RSQLite::dbWriteTable(my_connection,"customers"), customer_data)
